4874d89570a2a9cfd19a4b90a35bca432c1a2ace
manjunatha09 <manjuc.28.06@gmail.com>
2026-05-17 19:42:02 +0530

    Add files via upload

---
+++ userdata1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt update -y
+apt install -y apache2 awscli
+
+systemctl start apache2
+systemctl enable apache2
+
+aws s3 cp s3://manjunath-terraform-project-2026/index1.html /var/www/html/index.html
+
+chmod 644 /var/www/html/index.html
